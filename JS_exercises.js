110a26a4ce082e18bc3a5ad076c781d0794f1036
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-07-07 18:29:48 -0700

    Add files via upload

---
+++ JS_exercises.js
@@ -0,0 +1,114 @@
+// document.getElementById("count").innerText = 5
+
+// let count = 5
+// count = count + 1
+// console.log(count)
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked (log it out)
+// change the count-el in the HTML to reflect the new count
+
+// let countEl = document.getElementById("count-el") // pass in arguments
+
+// let count = 0
+
+// function increment() {
+//     count += 1
+//     countEl.innerText = count
+// }
+
+// // 1. Create a function, save(), which logs out the count when it's called
+
+// function save() {
+//     console.log(count)
+// }
+
+
+// -----------------------------------------
+// function countdown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+
+// // Setting up the the race 
+
+// countdown()
+
+// // GO! 🏁
+// // Players are running the race 💨
+// // Race is finished! 🍾
+
+// // Get ready for a new race 
+
+// countdown()
+
+// -----------------------------------------
+
+// Create a function (you decide the name) that logs out the number 42 to the console
+// Call/invoke the function
+
+// function myLogger() {
+//     console.log(42)
+// }
+
+// myLogger()
+
+// -----------------------------------------
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// // Create a function that logs out the sum of all the lap times
+// function logLapTime() {
+//     let totalTime = lap1 + lap2 + lap3
+//     console.log(totalTime)
+// }
+
+// // console.log(totalTime)
+
+// // console.log(totalTime) will not work, because the variable totalTime is instanced in the function only and cannot be called outside. Therefor it is better to use logLapTime()
+
+// -----------------------------------------
+
+// let lapsCompleted = 0
+// function lapsIncrement() {
+//     lapsCompleted = lapsCompleted + 1
+// }
+// lapsIncrement()
+// lapsIncrement()
+// lapsIncrement()
+
+// console.log(lapsCompleted)
+
+// -----------------------------------------
+
+// Grab the welcome-el paragraph and store it in a variable called welcomeEl
+
+// let welcomeEl = document.getElementById("welcome-el")
+
+// // Create two variables (name & greeting) that contains your name
+// // and the greeting we want to render on the page
+
+// let name = "Indigo Vanderborgh"
+// let greeting = "Welcome back, "
+
+// // Render the welcome message using welcomeEl.innerText
+
+// welcomeEl.innerText = greeting + name
+
+// In HTML, it would have been:
+/* <html>
+    <head>
+    </head>
+    <body>
+        <p id="welcome-el"></p>
+        <script src="index.js"></script>
+    </body>
+</html> */
+
+// -----------------------------------------
