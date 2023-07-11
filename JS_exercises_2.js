fd4e30562366f031132d64f224372bd830a34c51
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-07-10 19:23:06 -0700

    Add files via upload

--- JS_exercises_2.js
+++ JS_exercises_2.js
@@ -60,3 +60,25 @@
 
 // -----------------------------------------------------------------------------------------
 
+// Basic implementation of functions between html and js indexes
+
+// let [insert creative name here] = document.getElementById(["id in html in string notation"])
+
+// function([creative name, place in html as a function]) {
+    // [insert creative name here].textContent = "string"
+// }
+
+// -----------------------------------------------------------------------------------------
+
+// 1. Create two variables, firstCard and secondCard. 
+// Set their values to a random number between 2-11
+
+// let firstCard = 4
+
+// let secondCard = 7
+
+// // 2. Create a variable, sum, and set it to the sum of the two cards
+
+// let sum = firstCard + secondCard
+
+// console.log(sum)
