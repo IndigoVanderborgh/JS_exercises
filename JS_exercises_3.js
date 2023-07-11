cc9e306ce10bf6ac7a1f7c4634b93950aa03c6e4
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-07-10 22:15:34 -0700

    Add files via upload

---
+++ JS_exercises_3.js
@@ -0,0 +1,49 @@
+// 7/10/23 conditional statements challenge
+
+// Check if the person is old enough to enter the nightclub (21)
+// Log a suitable message to the console in both cases
+
+// let age = 22
+
+// if less than 21 -> "You can not enter the club!"
+// else            -> "Welcome!"
+
+// if (age < 21) {
+//     console.log("You can not enter the club!")
+// } else {
+//     console.log("Welcome!")
+// }
+
+// challenge 2 age is changed, using less than equal to.
+
+// let age = 20
+
+// if less than 21 -> "You can not enter the club!"
+// else            -> "Welcome!"
+
+// if (age <= 21) {
+//     console.log("You can not enter the club!")
+// } else {
+//     console.log("Welcome!")
+// }
+
+// note the change in line 24, as the range has been altered. only works if set age is lower
+// than 20 (line 19)
+
+// challenge 3
+
+// Check if the person is elegible for a birthday card from the King! (100)
+
+// let age = 100
+
+// if less than 100    -> "Not elegible"
+// else if exactly 100 -> "Here is your birthday card from the King!"
+// else                -> "Not elegible, you have already gotten one"
+
+// if (age < 100) {
+//     console.log("Not elegible")
+// } else if (age === 100) {
+//     console.log("Here is your birthday card from the King!")
+// } else {
+//     console.log("Not elegible, you have already gotten one")
+// }
