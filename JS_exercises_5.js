53c2daa018460586b57f7f08bdc5a2bfd9ac51b4
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-07-31 15:57:18 -0700

    Add files via upload

--- JS_exercises_5.js
+++ JS_exercises_5.js
@@ -31,3 +31,14 @@ let greetingEl = document.getElementById("greeting-el")
 for (let i = 0; i < sentence.length; i++) {
     greetingEl.textContent += sentence[i] + " "
 }
+
+// for (let i = 0; i < 6; i += 1) {
+//     console.log(i)
+// }
+
+// Create a for loop that counts from 10 to 100 in steps of 10
+// Use console.log to log out the numbers
+
+for (let i = 10; i < 101; i += 10) {
+    console.log(i)
+}
